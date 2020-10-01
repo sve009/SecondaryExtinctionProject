c1d5844c1345358c7ad5aa2ad1c0784bd7890c4e
Sam Eagen <sve9milo@gmail.com>
2020-10-01 14:08:28 -0600

    Finished the generalized cascade model

--- src/models/generalizedcascade.jl
+++ src/models/generalizedcascade.jl
@@ -1,5 +1,9 @@
 using Random, Distributions
 
+# E[x] = 2C
+# E[n] = .5
+# E[L] = .5 * 2C = C
+# Expected proportion of edges should be C
 function generalized_cascade_model(S, C)
     G = fill(0, S, S)
     unif = Uniform()
@@ -10,7 +14,13 @@ function generalized_cascade_model(S, C)
 
     for i in 1:S
         n = niches[i]
-        x = rand(dist)
+        x = rand(dist) # E[x] = 2C
+
+        for j in 1:length(niches)
+            if niches[j] <= n && rand(unif) <= x # Expected less is .5 * S. 
+                G[j, i] = 1
+            end
+        end
     end
     return G
 end
